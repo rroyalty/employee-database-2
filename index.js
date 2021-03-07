1d0737383f487c829562170d963e3df89cc1e28a
rroyalty <rroyalty@gmail.com>
2021-03-07 13:58:51 -0500

    User validation working.

--- index.js
+++ index.js
@@ -1,21 +1,82 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
+const credentials = require('./config/config.json');
+const fs = require('fs');
+const path = require('path');
+const bcrypt = require('bcrypt');
 
-const connection = mysql.createConnection({
-  host: 'localhost',
+// General Menu
 
-  // Your port; if not 3306
-  port: 3306,
+const generalMenu = () => {
+    console.log(`Welcome to Ryan's Employee Database!`)
+    inquirer
+        .promopt({
+            name: 'selection',
+            type: 'rawlist',
+            message: 'What would you like to do?',
+            choices: [
+                'View Deparments',
+                'View Roles',
+                'View Employees',
+                'Add Department',
+                'Add Role',
+                'Add Employee',
+                
+        })
+}
 
-  // Your username
-  user: 'root',
+// Compare login credentials to stored config file.
+const compareHash = (username, password) => {
+    bcrypt.compare(username, credentials.username, function(err, result) {
+        if (!result) {
+            console.log("Bad username.");
+            process.exit(0);
+        }
+        bcrypt.compare(password, credentials.password, function(err, result) {
+            if (!result) {
+                console.log("Bad password.");
+                process.exit(0);
+            }
 
-  // Your password
-  password: 'youcannotpassword',
-  database: 'employee-database',
-});
+            const connection = mysql.createConnection({
+                host: 'localhost',
+              
+                // Your port; if not 3306
+                port: 3306,
+              
+                // Your username
+                user: username,
+              
+                // Your password
+                password: password,
+                database: 'employee-database',
+              });
 
-connection.connect((err) => {
-  if (err) throw err;
-  console.log("connection to database successful.");
-});
+            connection.connect((err) => {
+            if (err) throw err;
+            console.log("connection to database successful.\n");
+            });
+        })
+    })
+}
+
+// Login
+const access = () => {
+    inquirer
+        .prompt([
+            {
+            type: 'input',
+            name: 'username',
+            message: 'What is your mySQL username?'
+            },
+            {
+            type: 'input',
+            name: 'password',
+            message: 'What is your mySQL password?'
+            }
+        ]).then((answer) => {
+                compareHash(answer.username, answer.password);
+            });
+}
+
+access();
